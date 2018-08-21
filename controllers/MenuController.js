7a54ba5eee394471bf845b046ff0743bf10963be
Matt Outen <mattoutenphysed@gmail.com>
2018-08-20 20:59:31 -0500

    Checkpoint create contacts and connect to database complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-
+const ContactController = require('./ContactController');
 module.exports = class MenuController {
     constructor(){
         this.mainMenuQuestions = [
@@ -14,7 +14,7 @@ module.exports = class MenuController {
                 ]
             }
         ];
-        this.contacts = [];
+        this.book = new ContactController();
     }
 
     main() {
@@ -48,8 +48,15 @@ module.exports = class MenuController {
 
     addContact() {
         this.clear();
-        console.log('addContact called');
-        this.main();
+       inquirer.prompt(this.book.addContactQuestions).then((answers) =>{
+           this.book.addContact(answers.name, answers.phone, answers.email).then((contact) =>{
+               console.log("Contact added successfully!");
+               this.main();
+           }).catch((err)=>{
+               console.log(err);
+               this.main();
+           });
+       });
     }
 
     exit() {
