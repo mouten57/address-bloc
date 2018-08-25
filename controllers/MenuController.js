288a555806f282af9a46925b44708e0a34287054
Matt Outen <mattoutenphysed@gmail.com>
2018-08-25 13:33:08 -0500

    list contacts and search checkpoint done

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,8 @@ module.exports = class MenuController {
                 message: 'Please choose from an option below: ',
                 choices: [
                     'Add new contact',
+                    'View all contacts',
+                    'Search for a contact',
                     'Get current date',
                     'Exit'
                 ]
@@ -21,8 +23,16 @@ module.exports = class MenuController {
         console.log('Welcome to AddressBloc!');
         inquirer.prompt(this.mainMenuQuestions).then((response) => {
             switch(response.mainMenuChoice) {
+                case "View all contacts":
+                    this.getContacts();
+                    break;
+
                 case "Add new contact":
                     this.addContact();
+                    break;
+
+                case "Search for a contact":
+                    this.search();
                     break;
 
                 case "Get current date":
@@ -59,6 +69,96 @@ module.exports = class MenuController {
        });
     }
 
+    getContacts(){
+        this.clear();
+
+        this.book.getContacts().then((contacts) => {
+            for (let contact of contacts) {
+                console.log(`
+                name: ${contact.name}
+                phone number: ${contact.phone}
+                email: ${contact.email}
+                ----------------`
+                );
+            }
+            this.main();
+        }).catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    search(){
+        inquirer.prompt(this.book.searchQuestions)
+        .then((target) => {
+            this.book.search(target.name)
+            .then((contact) => {
+                if(contact === null){
+                    this.clear();
+                    console.log("contact not found");
+                    this.search();
+                } else {
+                    this.showContact(contact);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    showContact(contact){
+        this._printContact(contact);
+        inquirer.prompt(this.book.showContactQuestions)
+        .then((answer) => {
+            switch(answer.selected){
+                case "Delete contact":
+                    this.delete(contact);
+                    break;
+                case "Main menu":
+                    this.main();
+                    break;
+                default:
+                    console.log("Something went wrong.");
+                    this.showContact(contact);
+            } 
+        })
+        .catch((err) => {
+            console.log(err);
+            this.showContact(contact);
+        });
+    }
+
+    _printContact(contact){
+        console.log(`
+            name: ${contact.name}
+            phone number: ${contact.phone}
+            email: ${contact.email}
+            ------------------------`
+        )
+    }
+
+    delete(contact){
+        inquirer.prompt(this.book.deleteConfirmQuestions)
+        .then((answer) => {
+            if(answer.confirmation){
+                this.clear();
+                this.book.delete(contact.id);
+                console.log(`contact deleted!
+                `);
+                this.main();
+            } else {
+                console.log("contact not deleted");
+                this.showContact(contact);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
+
     exit() {
         console.log('Thanks for using AddressBloc!');
         process.exit();
@@ -66,12 +166,17 @@ module.exports = class MenuController {
 
     getDate(){
         var currentdate = new Date(); 
+        var hours = currentdate.getHours();
+        if(hours > 12) hours -= 12
+        var amPM = (currentdate.getHours()) > 12 ? (" PM"): (" AM")
         var datetime = (currentdate.getMonth()+1)+ '/'
         + currentdate.getDate() + "/"
         + currentdate.getFullYear() + " @ "  
-        + currentdate.getHours() + ":"  
-        + currentdate.getMinutes()
-        console.log(datetime);
+        + hours + ":"  
+        + currentdate.getMinutes() + amPM
+        console.log(`
+        ${datetime}
+        `);
         this.main();
     }
 
